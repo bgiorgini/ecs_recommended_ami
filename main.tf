812ebfa71c8d389de208aea02c0d33af03473908
Bruno Giorgini <bruno.giorgini@autodesk.com>
2018-08-13 15:10:12 -0700

    draft for ecs_optimized_ami module
    Adding initial test for module to retrieve AWS recommended AMI for ecs-optimized instances.

---
+++ main.tf
@@ -0,0 +1,12 @@
+data "aws_ssm_parameter" "ecs_optimized_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux/recommended"
+}
+
+provider "aws" {
+  version = "~> 1"
+  region  = "us-west-2"
+}
+
+output "ecs_optimized_ami" {
+  value = "${data.aws_ssm_parameter.ecs_optimized_ami.value}"
+}
