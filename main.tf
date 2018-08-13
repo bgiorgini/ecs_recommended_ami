831fd4550c01d9c74a20021a87d298adf53d96a5
Bruno Giorgini <bruno.giorgini@autodesk.com>
2018-08-13 15:23:38 -0700

    Removing test provider to make it generic and reusable.

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ data "aws_ssm_parameter" "ecs_optimized_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux/recommended/image_id"
 }
 
-provider "aws" {
-  version = "~> 1"
-  region  = "us-west-2"
-}
-
 output "ecs_optimized_ami" {
   value = "${data.aws_ssm_parameter.ecs_optimized_ami.value}"
 }
